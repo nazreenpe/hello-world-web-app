86d942003aeabc0f54b898ca5909b155c96fa3d6
Nasreen Habeeb Ur Rahiman <nazreenpe@gmail.com>
2018-07-24 16:43:44 -0700

    Added endpoint /hello/{name}

--- src/main/java/com/nazreenpe/hello_world_practice/resource/HelloResource.java
+++ src/main/java/com/nazreenpe/hello_world_practice/resource/HelloResource.java
@@ -1,5 +1,6 @@
 package com.nazreenpe.hello_world_practice.resource;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,10 @@ public class HelloResource {
     @RequestMapping("")
     public String hello() {
         return "Hello World!";
+    }
+
+    @RequestMapping("/{name}")
+    public String helloName(@PathVariable("name") String name) {
+        return String.format("Hello %s, wazzup?", name);
     }
 }
