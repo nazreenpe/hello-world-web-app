0fcbdd536e04a017847deb8a2192fec106d55815
Nasreen Habeeb Ur Rahiman <nazreenpe@gmail.com>
2018-07-26 12:19:12 -0700

    Added HTTP response status code to /time/{format} endpoint

--- src/main/java/com/nazreenpe/hello_world_practice/resource/TimeResource.java
+++ src/main/java/com/nazreenpe/hello_world_practice/resource/TimeResource.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
@@ -20,13 +21,14 @@ public class TimeResource {
     }
 
     @RequestMapping(path = "/{format}", consumes = "application/json", produces = "application/json")
-    public Map<String, String > formatTime(@PathVariable("format") String format) {
+    public Map<String, String > formatTime(@PathVariable("format") String format, HttpServletResponse response) {
         try {
             String time = LocalDateTime.now().format(DateTimeFormatter.ofPattern(format));
             Map map = new HashMap<String, String>();
             map.put("datetime", time);
             return map;
         } catch (Exception e) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
             Map errorMessage = new HashMap<String, String>();
             errorMessage.put("Error", "Wrong Format");
             return errorMessage;
