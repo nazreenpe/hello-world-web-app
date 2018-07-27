4098c95040b2ebf9d7c19dfd1a3ed204960e0108
Nasreen Habeeb Ur Rahiman <nazreenpe@gmail.com>
2018-07-27 11:16:33 -0700

    Added Http Status Codes to all the endpoints

--- src/main/java/com/nazreenpe/hello_world_practice/resource/HelloResource.java
+++ src/main/java/com/nazreenpe/hello_world_practice/resource/HelloResource.java
@@ -1,27 +1,39 @@
 package com.nazreenpe.hello_world_practice.resource;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.Map;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloResource {
-    @RequestMapping(path = "", consumes = "application/json", produces = "application/json")
+    @RequestMapping(method = RequestMethod.GET, path = "", consumes = APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public Map<String, String> hello() {
         Map map = new HashMap<String, String>();
         map.put("Message", "Hello World!");
         return map;
     }
 
-    @RequestMapping(path = "/{name}", consumes = "application/json", produces = "application/json")
-    public Map<String, String> helloName(@PathVariable("name") String name) {
+    @RequestMapping(path = "/{name}", produces = "application/json")
+    public Map<String, String> helloName(@PathVariable("name") String name,
+                                         @RequestHeader("Content-Type") String contentType,
+                                         HttpServletResponse response) {
         String message = String.format("Hello %s", name);
         Map map = new HashMap<String, String>();
-        map.put("message", message);
+        if (contentType.equals("application/json")) {
+            map.put("message", message);
+            response.setStatus(200);
+            return map;
+        } else {
+            response.setStatus(415);
+            map.put("HTTP Status Code", "Unsupported Media Type");
+        }
         return map;
     }
 
--- src/main/java/com/nazreenpe/hello_world_practice/resource/TimeResource.java
+++ src/main/java/com/nazreenpe/hello_world_practice/resource/TimeResource.java
@@ -1,9 +1,10 @@
 package com.nazreenpe.hello_world_practice.resource;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -14,6 +15,7 @@ import java.util.Map;
 @RequestMapping("/time")
 public class TimeResource {
     @RequestMapping("")
+    @ResponseStatus(HttpStatus.OK)
     public String getTime() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm a");
         String localDateTime = LocalDateTime.now().format(formatter);
@@ -21,7 +23,13 @@ public class TimeResource {
     }
 
     @RequestMapping(path = "/{format}", consumes = "application/json", produces = "application/json")
-    public Map<String, String > formatTime(@PathVariable("format") String format, HttpServletResponse response) {
+    public Map<String, String > formatTime(@PathVariable("format") String format,
+                                           HttpServletResponse response,
+                                           HttpServletRequest request) {
+        if (!request.getMethod().equals(RequestMethod.GET.name())) {
+            response.setStatus(405);
+            return null;
+        }
         try {
             String time = LocalDateTime.now().format(DateTimeFormatter.ofPattern(format));
             Map map = new HashMap<String, String>();
